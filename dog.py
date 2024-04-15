945b4c7552db2ebd279328e288ee131af96612d2
Michael Beavers <33707857+mbeavers34@users.noreply.github.com>
2024-04-15 09:51:22 -0500

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,18 +1,21 @@
-class Dog:
-     species = "Canis familiaris"
+# Header---------------------------------------------------
+# Functions------------------------------------------------
+class Dog:                                        # Create class
+     species = "Canis familiaris"                 # Inherited   
      def __init__(self, name, age, sound):
-         self.name = name
-         self.age = age
-         self.sound = sound   
-     def bark(self):
+         self.name = name                         # Attribute
+         self.age = age                           # Attribute   
+         self.sound = sound                       # Attribute     
+     def bark(self):                              # Method
          print(self.sound)
-         
-miles = Dog("Miles", 4, "woof")
-buddy = Dog("Buddy", 9, "")
-buddy.sound ="yip yip"
+# Variables-----------------------------------------------
 
+# Main---------------------------------------------------
+#miles = Dog("Miles", 4, "woof")                   # objects
+buddy = Dog("Buddy", 9, "")                       # objects
+#buddy.sound ="yip yip"
 
 print(buddy.age)
-print(miles.species)
-buddy.bark()
-miles.bark()
+#print(miles.species)
+#buddy.bark()
+#miles.bark()
