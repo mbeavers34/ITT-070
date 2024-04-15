83eed61d1b064ecf785863c991c36ce3ad95e2d6
Michael Beavers <33707857+mbeavers34@users.noreply.github.com>
2024-04-15 12:19:06 -0500

    Update calc3.py

--- calc3.py
+++ calc3.py
@@ -1,22 +1,30 @@
-
 # Header------------------------------------------
+#Name Michael Beavers
+#Date 4/15/2024
+#Program: calc3.py
+#Description: Simple text base calculator
 # Functions --------------------------------------
-class cal():                               #Create calculator class
+class calc():                               #Create calculator class
     def __init__(self,a,b):
         self.a=a                           # properties of the object
         self.b=b                           # properties of the object
+  
     def add(self):                         # Dont need properties they are inherited 
         return self.a+self.b
-    def mul(self):
-        return self.a*self.b
-    def div(self):
-        return self.a/self.b
+  
     def sub(self):
         return self.a-self.b
+  
+    def mul(self):
+        return self.a*self.b
+  
+    def div(self):
+        return self.a/self.b
+    
 #Main------------------------------------------------
-a=int(input("Enter first number: "))
-b=int(input("Enter second number: "))
-equation=cal(a,b)                           # create the object
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+equation=calc(a,b)                           # create the object
 choice=1
 while choice!=0:
     print("0. Exit")
@@ -26,17 +34,16 @@ while choice!=0:
     print("4. Division")
     choice=int(input("Enter choice: "))
     if choice==1:
-        print("Result: ",equation.add())
+        print(f'\n {a}+{b}={equation.add()}\n')
     elif choice==2:
-        print("Result: ",equation.sub())
+        print(f'\n {a}-{b}={equation.sub()}\n')
     elif choice==3:
-        print("Result: ",equation.mul())
+        print(f'\n {a}+{b}={equation.mul()}\n')
     elif choice==4:
-              print("Result: ",round(equation.div(),2))
+        print(f'\n {a}+{b}={round(equation.div(),3)}\n')
     elif choice==0:
         print("Exiting!")
     else:
-        print("Invalid choice!!")
- 
+        print("Not a valid choice. Please try again!!")
  
 print()
